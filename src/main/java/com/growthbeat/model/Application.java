a9971864cca89cad1770834b6e08fb3129d840e8
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-06-17 22:35:23 +0900

    Add Application and Client class

---
+++ src/main/java/com/growthbeat/model/Application.java
@@ -0,0 +1,35 @@
+package com.growthbeat.model;
+
+import java.util.Date;
+
+public class Application extends Model {
+
+	private String id;
+	private String name;
+	private Date created;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+}
---
+++ src/main/java/com/growthbeat/model/Client.java
@@ -0,0 +1,35 @@
+package com.growthbeat.model;
+
+import java.util.Date;
+
+public class Client extends Model {
+
+	private String id;
+	private Date created;
+	private Application application;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Application getApplication() {
+		return application;
+	}
+
+	public void setApplication(Application application) {
+		this.application = application;
+	}
+
+}
