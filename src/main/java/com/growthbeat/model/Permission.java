d1fa3e7a7edfb21084190618ad2c5d3271bf8ee9
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-06-17 22:35:07 +0900

    Add Resource, Action and Permission model class

---
+++ src/main/java/com/growthbeat/model/Permission.java
@@ -0,0 +1,53 @@
+package com.growthbeat.model;
+
+import java.util.Date;
+
+public class Permission extends Model {
+
+	private Date created;
+	private Account account;
+	private Account targetAccount;
+	private Resource resource;
+	private Action action;
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public Account getTargetAccount() {
+		return targetAccount;
+	}
+
+	public void setTargetAccount(Account targetAccount) {
+		this.targetAccount = targetAccount;
+	}
+
+	public Resource getResource() {
+		return resource;
+	}
+
+	public void setResource(Resource resource) {
+		this.resource = resource;
+	}
+
+	public Action getAction() {
+		return action;
+	}
+
+	public void setAction(Action action) {
+		this.action = action;
+	}
+
+}
---
+++ src/main/java/com/growthbeat/model/Resource.java
@@ -0,0 +1,44 @@
+package com.growthbeat.model;
+
+import java.util.Date;
+
+public class Resource extends Model {
+
+	private String id;
+	private String name;
+	private Date created;
+	private Resource parentResource;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Resource getParentResource() {
+		return parentResource;
+	}
+
+	public void setParentResource(Resource parentResource) {
+		this.parentResource = parentResource;
+	}
+
+}
