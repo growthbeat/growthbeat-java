045698eff0480160851b8521c625faed6c0f0879
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-06-19 15:37:34 +0900

    Implement Credential#getByConnectionIdAndSecretService method

--- src/main/java/com/growthbeat/model/Credential.java
+++ src/main/java/com/growthbeat/model/Credential.java
@@ -1,12 +1,30 @@
 package com.growthbeat.model;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.codehaus.jackson.type.TypeReference;
 
 public class Credential extends Model {
 
 	private String id;
 	private Date created;
 	private Account account;
+
+	public static List<Credential> getByConnectionIdAndSecretService(String connectionId, String secretService) {
+
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("connectionId", connectionId);
+		params.put("secretService", secretService);
+
+		List<Credential> credentials = get(1, "credentials", params, new TypeReference<List<Credential>>() {
+		});
+
+		return credentials;
+
+	}
 
 	public String getId() {
 		return id;
