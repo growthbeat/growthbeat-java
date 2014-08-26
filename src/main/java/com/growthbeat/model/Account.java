cd691a07d3f82ceca559a3b2cf5f51ce938b47e5
Shigeru Ogawa <ogawa@sirok.co.jp>
2014-08-26 16:04:35 +0900

    Add get list accout method.

--- src/main/java/com/growthbeat/model/Account.java
+++ src/main/java/com/growthbeat/model/Account.java
@@ -2,7 +2,10 @@ package com.growthbeat.model;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import org.codehaus.jackson.type.TypeReference;
 
 import com.growthbeat.Context;
 
@@ -38,6 +41,13 @@ public class Account extends Model {
 
 	public static Account findById(String id, Context context) {
 		return get(context, String.format("1/accounts/%s", id), new HashMap<String, Object>(), Account.class);
+	}
+
+	public static List<Account> findListById(String id, Context context) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", id);
+		return get(context, "1/accounts/", params, new TypeReference<List<Account>>() {
+		});
 	}
 
 	public static Account create(String name, Context context) {
